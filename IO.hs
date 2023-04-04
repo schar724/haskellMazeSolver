71f66af826e1833e6a602f7a00e2384e3c4e958a
schar724 <scotty.charles@gmail.com>
2023-04-03 21:46:44 -0600

    first passed test

--- IO.hs
+++ IO.hs
@@ -1,35 +1,44 @@
-module IO (readMaze, printSolution) where
+module IO (printSolutions) where
 import DataTypes
 
 
 
-readMaze :: FilePath -> IO Maze
-readMaze filePath = do
-  contents <- readFile filePath
-  let maze = map (map read . words) $ lines contents
-  if (length maze == 0) || (length (head maze) == 0) then
-    error "File is empty"
-  else if (length $ filter (\x -> x `notElem` [0,1,8,9]) $ concat maze) > 0 then
-    error "File contains invalid characters"
-  else
-    return maze
+-- readMaze :: String -> Maze
+-- readMaze contents = do
+--   let maze = map (map read . words) $ lines contents
+--   if (length maze == 0) || (length (head maze) == 0) then
+--     error "File is empty"
+--   else if (length $ filter (\x -> x `notElem` [0,1,8,9]) $ concat maze) > 0 then
+--     error "File contains invalid characters"
+--   else
+--     return maze
 
-printSolution :: [[Coord]] -> IO ()
-printSolution solutions =
-  mapM_ (\solution -> mapM_ putStrLn (createMaze solution ++ [""])) solutions
+-- printSolution :: [[Coord]] -> IO ()
+-- printSolution solutions =
+--   mapM_ (\solution -> mapM_ putStrLn (createMaze solution ++ [""])) solutions
 
--- | Takes a list of coordinates representing a solution path through a maze, and returns a list of strings
---   representing the maze with walls represented by '#' and the path represented by 'X'. The start and exit
---   points are also represented by 'X'.
-createMaze :: [Coord] -> [String]
-createMaze solution =
-  let (maxX, maxY) = foldl (\(mx, my) (x, y) -> (max mx x, max my y)) (0, 0) solution
-      isPath x y = (x, y) `elem` solution
-      isStartOrExit x y = (x, y) == head solution || (x, y) == last solution
-      isWall x y = not (isPath x y || isStartOrExit x y)
-      charFor x y
-        | isWall x y = '#'
-        | isStartOrExit x y = 'X'
-        | isPath x y = 'X'
-        | otherwise = ' '
-  in [ [ charFor x y | x <- [0..maxX] ] | y <- [0..maxY] ]
+-- -- | Takes a list of coordinates representing a solution path through a maze, and returns a list of strings
+-- --   representing the maze with walls represented by '#' and the path represented by 'X'. The start and exit
+-- --   points are also represented by 'X'.
+-- createMaze :: [Coord] -> [String]
+-- createMaze solution =
+--   let (maxX, maxY) = foldl (\(mx, my) (x, y) -> (max mx x, max my y)) (0, 0) solution
+--       isPath x y = (x, y) `elem` solution
+--       isStartOrExit x y = (x, y) == head solution || (x, y) == last solution
+--       isWall x y = not (isPath x y || isStartOrExit x y)
+--       charFor x y
+--         | isWall x y = '#'
+--         | isStartOrExit x y = 'X'
+--         | isPath x y = 'X'
+--         | otherwise = ' '
+--   in [ [ charFor x y | x <- [0..maxX] ] | y <- [0..maxY] ]
+
+printSolutions :: [[Int]] -> [[Coord]] -> IO ()
+printSolutions maze paths = do
+  let maxX = length maze - 1
+      maxY = length (head maze) - 1
+  mapM_ (\path -> do
+            let maze' = [[if (x,y) `elem` path then 'X' else if (maze !! x !! y) == 0 then '#' else if (x,y) == (0,0) then '8' else if (x,y) == (maxX,maxY) then '9' else ' ' | y <- [0..maxY]] | x <- [0..maxX]]
+            mapM_ putStrLn maze'
+            putStrLn ""
+        ) paths
--- Main.hs
+++ Main.hs
@@ -1,12 +1,17 @@
 
-import MazeBuilder
-import Solver
-import DataTypes
-import IO
+import MazeBuilder ( buildMazeData )
+import Solver ( solve )
+import DataTypes ()
+import IO (printSolutions )
+import System.Environment
+import System.IO
 
-main :: String -> IO ()
-main path =
-    do p <- readPath path
-    case solve (buildMazeData p) of
-        [] -> putStr "no solution"
-        solution -> printSolution solution 
+main :: IO ()
+main = do
+    args <- getArgs
+    file <- openFile (head args) ReadMode
+    text <- hGetContents file
+    let maze = map (map read . words) $ lines text 
+    let mazeData = buildMazeData maze
+    let solutions = solve mazeData
+    printSolutions maze solutions
